dac57457a21c7fe9185ecc9275a71a78eeae136d
pawel <devnull@localhost>
2012-04-22 23:27:49 +0200

    simplified refreshing process, fixed a Chrome bug

--- jquery.f5-0.2.js
+++ jquery.f5-0.2.js
@@ -1,18 +1,16 @@
-/*!
- * f5 - autorefresh jQuery plugin v1.4.2
- * http://code.google.com/p/f5-autorefresh/
+/**
+ * f5-autorefresh
  *
- * Copyright 2010, Pawel Turlejski
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
+ * (c) copyright 2010-2012, tengidesign.com / Pawel Turlejski
+ * Licensed under the MIT License at:
+ * 		http://www.opensource.org/licenses/mit-license.php
  *
- * Date: Friday July 23 2010 
  */
-(function($) {
+;(function($) {
     $.f5 = function(options) {
         var settings = $.extend({
 
-            css: 'auto',                                // List of CSS files to monitor:
+            css: 'all',                                 // List of CSS files to monitor:
                                                         //   - 'auto'                       - monitor all <link>ed CSS files
                                                         //   - ['css/s1.css', 'css/s2.css'] - monitor s1 and s2
                                                         //   - [] or false                  - don't monitor any CSS
@@ -21,33 +19,19 @@
 
             timeout: 750,                               // Refresh rate in milliseconds.
 
-            start: true,                                // Start refreshing loop on load.
+            autostart: true,                            // Start refreshing loop on load.
 
-            toggle: { tag: 'body', event: 'dblclick' }  // Toggle updates' monitoring by this action.
+            toggle: {tag: 'body', event: 'dblclick'}    // Toggle updates' monitoring by this action.
                                                         // Set toggle: false to disable.
         }, options)
-
+        
         var _fetch_css = function (call_after, css_idx) {
             if (css_idx < _css.length) {
                 var css = _css[css_idx];
                 var xhr = $.ajax({url: css.url, cache: false, type: 'GET', success: function(content) {
                     var cur_updated = xhr.getResponseHeader('Last-Modified')
                     if (css.updated != cur_updated && css.updated != -1) {
-                        // IE hack: for some reason IE refuses to update text inside a <style> tag,
-                        //          so just reload the whole page
-                        if ($.browser.msie) {
-                            window.location.reload()
-                            return;
-                        }
-                        else {
-                            // set different title for every css, so they can be updated individually
-                            var title = 'Css' + css_idx
-                            // try to disable it's original version that could be included via <link>
-                            $("link[href*=" + css.url + "]").each(function(){ this.disabled = true })
-                            // insert updated css into a new <style> tag
-                            $("style[title=" + title + "]").remove();
-                            $("<style title='" + title + "' type='text/css'>").text(content).appendTo($('head'));
-                        }
+                        window.location.reload()
                     }
                     css.updated = cur_updated
                     _fetch_css(call_after, css_idx + 1)
@@ -76,38 +60,43 @@
             }
         }
 
-        var _start = function() {
+        var _run = function() {
             _running = false
             if (_paused)
                 return;
             _running = true
-            setTimeout(function () { _fetch_html(_start) }, settings.timeout);
+            setTimeout(function () {_fetch_html(_run)}, settings.timeout);
         }
 
         var _toggle = function () {
             _paused = !_paused
             if (!_running)
-                _start()
+                _run()
         }
 
-        var _paused = !settings.start, 
+        var _paused = !settings.autostart, 
             _running = false,
-            _need_reload = false;
-            _html = settings.html ? { url: '', updated: -1 } : false, 
+            _html = settings.html ? {url: '', updated: -1} : false, 
             _css = []
 
-        if (settings.css === 'auto') {
-            $("link[rel=stylesheet]").each(function(){ _css[_css.length] = { url: this.href, updated: -1 } })
+        if (settings.css === 'all') {
+            $("link[rel=stylesheet]").each(function(){ 
+                // for some reason Chrome adds that weird css with href="data:text/css"...
+                if (this.href.indexOf('data:') > -1) {
+                    return
+                }
+                _css[_css.length] = {url: this.href, updated: -1};
+            })
         }
         else {
             for (var i in settings.css)
-                _css[_css.length] = { url: settings.css[i], updated: -1 }
+                _css[_css.length] = {url: settings.css[i], updated: -1}
         }
 
         if (settings.toggle)
-            $(settings.toggle.tag).bind(settings.toggle.event, function(){ _toggle() })
+            $(settings.toggle.tag).bind(settings.toggle.event, function(){_toggle()})
 
-        _start()
+        _run()
         
         return $;
     }
